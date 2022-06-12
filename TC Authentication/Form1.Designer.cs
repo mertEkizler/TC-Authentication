400044772a76c545f2b58e00a446e8c7b3a704a4
mert <mert@DESKTOP-D7EVAFE>
2022-06-12 22:14:55 +0300

    Add project files.

---
+++ TC Authentication/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TC_Authentication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtBirth = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "FIRSTNAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "LASTNAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "YEAR OF BIRTH";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(131, 17);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(205, 20);
+            this.txtTC.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(131, 54);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(205, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(131, 91);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(205, 20);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // txtBirth
+            // 
+            this.txtBirth.Location = new System.Drawing.Point(131, 128);
+            this.txtBirth.Name = "txtBirth";
+            this.txtBirth.Size = new System.Drawing.Size(205, 20);
+            this.txtBirth.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(185, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "VERIFY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 226);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBirth);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Authentication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtBirth;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TC Authentication/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TC_Authentication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var identificationNumber = long.Parse(txtTC.Text);
+            var birthOfYear = int.Parse(txtBirth.Text);
+            bool situation;
+
+            try
+            {
+                using (TCVerify.KPSPublicSoapClient service = new TCVerify.KPSPublicSoapClient { })
+                {
+                    situation = service.TCKimlikNoDogrula(identificationNumber, txtName.Text, txtLastname.Text, birthOfYear);
+                }
+
+                if (situation == true)
+                {
+                    MessageBox.Show("Valid user.");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid user.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentNullException(ex.Message);
+            }
+        }
+    }
+}
